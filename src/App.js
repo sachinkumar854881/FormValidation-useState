76de99a200fa74d6e38ed7c51f2b39429f947b62
sachin <sachinkumar854881@gmail.com>
2024-03-09 00:34:30 +0530

    add files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+
+    const [name,setName] = useState("sachin")
+    const [age,setAge] = useState("")
+
+    function collectName(event){
+        setName(event.target.value)
+    }
+    function collectAge(event){
+        setAge(event.target.value)
+    }
+
+    function validateData(event){
+        event.preventDefault();
+        if(!name)
+        {
+            console.log("please enter name!!!")
+        }
+        if(!age)
+        {
+            console.log("please enter age!!!")
+        }
+    }
+
+    return(
+        <div>
+            <center>
+                <form method="POST" onSubmit={validateData}>
+                    <h2>Contact form</h2>
+        
+                    <label>Name:</label>
+                    <input type="text" value={name} onChange={collectName}/><br/><br/>
+                    <label>Age:</label>
+                    <input type="number" value={age} onChange={collectAge}/><br/><br/>
+                    <button>Submit</button>
+                </form>
+            </center>
+        </div>
+    )
 }
-
 export default App;
